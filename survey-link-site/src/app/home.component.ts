f45ccd9a08043a7a51d6e67ac0bc3248de021aed
21797545 <u21797545@tuks.co.za>
2024-10-08 09:56:04 +0200

    Updated UI & added tailwind

--- survey-link-site/src/app/home.component.ts
+++ survey-link-site/src/app/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  imports: [
+    NgOptimizedImage
+  ],
 })
 export class HomeComponent {
   surveyLink: string = 'https://your-google-form-link-here';
+  currentYear: number = new Date().getFullYear();
 }
